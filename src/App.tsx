036874314d89d749c9bf91d171b26a8ff4438730
Augusto Pastrana <apastrana@auntap.com>
2021-07-25 18:30:43 -0300

    v.0.0.7

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Router, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Madisonpage from "./pages/madisonpage";
 import Homepage from "./pages/homepage";
 
